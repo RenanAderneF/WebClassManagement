6799945aa14d809e102d65689d94cb3a6e87f892
Renan <108879679+RenanAderneF@users.noreply.github.com>
2025-03-12 17:52:47 -0300

    Conexão com banco de dados realizada

--- api/server.js
+++ api/server.js
@@ -1,19 +1,32 @@
 import express from 'express';
+import 'dotenv/config';
+import { neon } from '@neondatabase/serverless';
 import cors from 'cors';
-import turmaRouter from './routes/turma.js'
+import turmaRouter from './routes/turma.js';
 
+//Cria aplicação Express:
 const app = express();
+const PORT = process.env.PORT || 4242;
+
+//Cria conexão com banco ao receber solicitação GET na rota raiz, ou seja, ao iniciar a api.
+app.get('/', async (_, res) => {
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const response = await sql`SELECT version()`;
+    const { version } = response[0];
+    res.json({ version });
+  });
 
 //Permite comunicação Cross-Origin:
 app.use(cors());
 
+//Roteadores importados:
 app.use("/turmas", turmaRouter);
 
-
-app.listen(3000, (error) => {
+//Inicia conexão:
+app.listen(PORT, (error) => {
 
     if(!error) {
-        console.log("Servidor iniciado na porta 3000");
+        console.log("Servidor iniciado na porta", PORT);
     } else {
         console.log("Erro ao conectar servidor", error);
     }
